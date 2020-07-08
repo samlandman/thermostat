bc73dd0bb595fc65adc1a327679c50031282460a
sam landman <sam landman>
2020-07-08 16:19:45 +0100

    Guard on minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,4 +19,16 @@ describe('Thermostat', function(){
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
+  it('decreases the temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    for (var i=0; i < 11; i++) {
+    thermostat.down();
+    };
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,6 +2,7 @@
 
 class Thermostat{
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this._temperature = 20;
   };
   getCurrentTemperature() {
@@ -10,5 +11,18 @@ class Thermostat{
 
   up() {
     this._temperature += 1 ;
+  };
+
+  down() {
+    console.log(this.isMinimumTemperature)
+    if (this.isMinimumTemperature()) {
+      return;
+      } {
+      this._temperature -= 1;
+    };
+  };
+
+  isMinimumTemperature() {
+    return(this._temperature === this.MINIMUM_TEMPERATURE);
   };
 };
